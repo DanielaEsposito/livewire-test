0a0274d665263ead4a39b6de4085f118ebc020c1
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-05-27 09:08:56 +0200

    count

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
     <div class="container text-center mx-auto p-4">
-        <h1 class="text-ceter my-5 text-white text-2xl">Counter {{ $count }}</h1>
+        <h1 class="text-ceter my-5 text-white text-2xl">Count {{ $count }}</h1>
         <button wire:click="increment"
             class="p-2 cursor-pointer hover:bg-indigo-700 mx-2 rounded-md  border-2 border-indigo-500 text-white">Incrementa</button>
         <button wire:click="decrement"
