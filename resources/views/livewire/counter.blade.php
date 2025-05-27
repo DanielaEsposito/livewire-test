f13b1134a8ec61eed508d96b349b6d4bb2ac5d1c
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-05-27 08:28:44 +0200

    add livewire view counter component

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,8 +2,11 @@
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
     <div class="container text-center mx-auto p-4">
         <h1 class="text-ceter my-5 text-white text-2xl">Counter {{ $count }}</h1>
-        <button class="p-2 rounded-md  border-2 border-indigo-500 text-white">Incrementa</button>
-        <button class="p-2 rounded-md  border-2 border-indigo-500 text-white">Decrementa</button>
-        <button class="p-2 rounded-md  border-2 border-indigo-500 text-white">Reset</button>
+        <button wire:click="increment"
+            class="p-2 cursor-pointer hover:bg-indigo-700 mx-2 rounded-md  border-2 border-indigo-500 text-white">Incrementa</button>
+        <button wire:click="decrement"
+            class="p-2 cursor-pointer hover:bg-indigo-700 mx-2 rounded-md  border-2 border-indigo-500 text-white">Decrementa</button>
+        <button wire:click="resetCount"
+            class="p-2 cursor-pointer hover:bg-indigo-700 mx-2 rounded-md  border-2 border-indigo-500 text-white">Reset</button>
     </div>
 </div>
